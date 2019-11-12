acdd5cf6aaa946f06fc24ec32f6646abdc82cb98
Gavin Dreyer <gavindreyer1@gmail.com>
2019-11-12 15:57:25 -0800

    auth with cookies

--- auth/authMW.js
+++ auth/authMW.js
@@ -3,22 +3,29 @@ const Users = require('../users/users-model');
 const bcrypt = require('bcryptjs');
 
 module.exports = (req, res, next) => {
-	let { username, password } = req.headers;
-	if (username && password) {
-		Users.findBy({ username })
-			.first()
-			.then(user => {
-				if (user && bcrypt.compareSync(password, user.password)) {
-					next();
-				} else {
-					res.status(401).json({ message: 'Invalid Credentials' });
-				}
-			})
-			.catch(error => {
-				console.log(error);
-				res.status(500).json(error);
-			});
+	// let { username, password } = req.headers;
+	// if (username && password) {
+	// 	Users.findBy({ username })
+	// 		// .first()
+	// 		.then(user => {
+	// 			console.log(user[0]);
+	// 			if (user[0] && bcrypt.compareSync(password, user[0].password)) {
+	// 				next();
+	// 			} else {
+	// 				res.status(401).json({ message: 'Invalid Credentials' });
+	// 			}
+	// 		})
+	// 		.catch(error => {
+	// 			console.log(error);
+	// 			res.status(500).json(error);
+	// 		});
+	// } else {
+	// 	res.status(400).json({ message: 'please provide credentials' });
+	// }
+
+	if (req.session && req.session.username) {
+		next()
 	} else {
-		res.status(400).json({ message: 'please provide credentials' });
+		res.status(401).json({ you: 'shall not pass' })
 	}
 };
--- server.js
+++ server.js
@@ -2,10 +2,36 @@ const express = require('express');
 const Users = require('./users/users-model');
 const authMidW = require('./auth/authMW');
 const bcrypt = require('bcryptjs');
+const cors = require('cors');
+const session = require('express-session')
+const KnexSessionStorage = require('connect-session-knex')(session)
+
+const knexConnection = require('./database/dbConfig')
 
 const server = express();
 
+const sessionConfiguration = {
+	name: 'mySesh',
+	secret: process.env.COOKIE_SECRET || 'it is secret! it is safe!',
+	cookie: {
+		maxAge: 1000 * 60 * 60,
+		secure: process.env.NODE_ENV === 'development' ? false : true,
+		httpOnly: true
+	},
+	resave: false,
+	saveUninitialized: true,
+	store: new KnexSessionStorage({
+		knex: knexConnection,
+		clearInterval: 1000 * 60 * 10,
+		tablename: 'user_sessions',
+		sidfieldname: 'id',
+		createtable: true
+	})
+}
+
 server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfiguration))
 
 server.post('/api/register', (req, res) => {
 	let user = req.body;
@@ -17,7 +43,7 @@ server.post('/api/register', (req, res) => {
 
 	Users.add(user)
 		.then(uInfo => {
-			console.log(uInfo);
+			req.session.username = uInfo.username;
 			res.status(201).json({ password: password, hash: user.password });
 		})
 		.catch(error => {
@@ -32,6 +58,7 @@ server.post('/api/login', (req, res) => {
 		.first()
 		.then(user => {
 			if (user && bcrypt.compareSync(password, user.password)) {
+				req.session.username = user.username
 				res.status(200).json({ message: `Welcome ${user.username}!` });
 			} else {
 				res.status(401).json({ message: 'Invalid Credentials' });
@@ -49,5 +76,18 @@ server.get('/api/users', authMidW, (req, res) => {
 		})
 		.catch(err => res.send(err));
 });
+
+server.get('/logout', (req, res) => {
+	if (req.session) {
+		req.session.destroy(err => {
+			if (err) {
+				res.status(500).json({ error: err })
+			}
+		})
+		res.status(200).json({ you: 'logged out successfully' })
+	} else {
+		res.status(200).json({ you: 'are already logged out' })
+	}
+})
 
 module.exports = server;
